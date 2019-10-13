dc7b17b1b01a0fcf1c59cb58bddacdfea3e5a064
Adrien <39187559+acrovato@users.noreply.github.com>
2019-10-13 18:22:04 +0200

    Add dynamic theme support and new preference page

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:preferences/preferences.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
 
 import 'router.dart';
 
-void main() => runApp(Fluttair());
+main() async {
+  await PrefService.init(prefix: 'pref_');
+  runApp(Fluttair());
+}
 
 class Fluttair extends StatelessWidget {
-  ThemeData lightTheme() {
-    return ThemeData(
-      // Define the default brightness and colors.
-      brightness: Brightness.light,
-      dividerColor: Colors.black,
-      accentColor: Colors.lightBlue,
-      primaryColor: Colors.lightBlue,
-      primaryColorDark: Colors.blue,
-      hintColor: Colors.grey,
-      // Define the default font family.
-      fontFamily: 'Montserrat',
-      // Define the default TextTheme. Use this to specify the default
-      // text styling for headlines, titles, bodies of text, and more.
-      textTheme: TextTheme(
-        headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-        title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-        body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-      ),
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'FluttAir',
-        theme: lightTheme(),
-        darkTheme: Theme.of(context).copyWith(brightness: Brightness.dark),
-        routes: myRoute,
-    );
+    return DynamicTheme(
+        defaultBrightness: Brightness.dark,
+        data: (brightness) => ThemeData(
+              brightness: brightness,
+              dividerColor: Colors.black,
+              accentColor: Colors.lightBlue,
+              primaryColor: Colors.lightBlue,
+              primaryColorDark: Colors.blue,
+              hintColor: Colors.grey,
+            ),
+        themedWidgetBuilder: (context, theme) {
+          return MaterialApp(
+            title: 'FluttAir',
+            theme: theme,
+            routes: myRoute,
+          );
+        });
   }
 }
--- lib/settings.dart
+++ lib/settings.dart
@@ -1,34 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:preferences/preferences.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
 
 import 'sidebar.dart';
 
-class SettingsView extends StatelessWidget {
+class SettingsView extends StatefulWidget {
+  SettingsView({Key key}) : super(key: key);
+
+  @override
+  SettingsViewState createState() => SettingsViewState();
+}
+
+class SettingsViewState extends State<SettingsView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: SideBar(),
-        appBar: AppBar(
-          title: Text('Settings'),
+      drawer: SideBar(),
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: PreferencePage([
+        PreferenceTitle('General'),
+        PreferenceTitle('Units'),
+        PreferenceDialogLink(
+          'Speed',
+          dialog: PreferenceDialog(
+            [
+              RadioPreference('knots', 'a', 'a', selected: true, isDefault: true),
+              RadioPreference('km/h', 'b', 'b', onSelect: () {},),
+              RadioPreference('mph', 'c', 'c')
+            ],
+            title: 'Speed',
+            cancelText: 'Cancel',
+          ),
         ),
-        body: ListView(
-            children: <Widget>[
-              CheckboxListTile(
-                value: false,
-                title: Text("This is a CheckBoxPreference"),
-                onChanged: (value) {},
-              ),
-              SwitchListTile(
-                value: false,
-                title: Text("This is a SwitchPreference"),
-                onChanged: (value) {},
-              ),
-              ListTile(
-                title: Text("This is a ListPreference"),
-                subtitle: Text("Subtitle goes here"),
-                onTap: (){},
-              )
-            ]
-        )
+        PreferenceTitle('Database'),
+        PreferenceTitle('Appearance'),
+        SwitchPreference('Dark theme', 'dark', defaultVal: false, onEnable: () {
+          DynamicTheme.of(context).setBrightness(Brightness.dark);
+        }, onDisable: () {
+          DynamicTheme.of(context).setBrightness(Brightness.light);
+        }),
+        PreferenceTitle('About')
+      ]),
     );
   }
 }
